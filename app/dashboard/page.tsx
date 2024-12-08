6d4874b688fcbcd17e807fe21bc835c8e0019fce
Hari Krishna <novach550@gmail.com>
2024-12-08 17:44:52 +0530

    Added Dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Dashboard from '@/app/components/dashboard/Dashboard';
+import Layout from '@/app/components/layout/Layout';
+
+export default function DashboardPage() {
+  return (
+    <Layout>
+      <Dashboard />
+    </Layout>
+  );
+} 
