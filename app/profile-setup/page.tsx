05bfc8ce7698b442b23cde3077f18e5f4c8e1eeb
Far El <36641995+pharaouk@users.noreply.github.com>
2024-12-08 10:29:42 +0400

    added login and sign up flows, functional but not integrated yet with auth (#5)

---
+++ app/profile-setup/page.tsx
@@ -0,0 +1,6 @@
+// app/profile-setup/page.tsx
+import ProfileSetup from '../components/auth/ProfileSetup';
+
+export default function ProfileSetupPage() {
+  return <ProfileSetup />;
+}
---
+++ app/signin/page.tsx
@@ -0,0 +1,7 @@
+// app/signin/page.tsx
+
+import SignIn from "../components/auth/SignIn";
+
+export default function SignInPage() {
+  return <SignIn />;
+}
